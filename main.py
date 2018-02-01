e47356a7afb4e08654a3c594fe423bb8f28b077f
Ariel Jimenez <ajimenez@intellisys.com.do>
2018-02-01 00:03:14 -0400

    Add templates and api endpoint (wip)

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return '<h1>Hi ariel</h1>'
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    return render()
+
+
+@app.route('/home')
+def home():
+    return render("Home")
+
+
+@app.route('/pages/<page>')
+def pages(page):
+    return render(page)
+
+
+@app.route('/login')
+def login():
+    return render(template="login")
+
+
+def render(var="World", template="template"):
+    return render_template(template +".j2", var=var)
+
+
+# API
+
+@app.route('/process', methods=['POST'])
+def proc():
+    return ''
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    """
+    by default host=localhost and port=5000
+    """
+    app.run(debug=True)
